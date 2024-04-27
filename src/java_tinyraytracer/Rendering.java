318fd8c52e69199938369e15bf2954576341abc7
shobhit <shobhit32t@gmail.com>
2024-04-27 19:56:20 +0530

    Create a simple .ppm by writing appropriate header to the created file

--- src/java_tinyraytracer/Rendering.java
+++ src/java_tinyraytracer/Rendering.java
@@ -5,7 +5,9 @@ import java.util.Formatter;
 public class Rendering {
 	public void createFile() {
 		try {
-			Formatter form = new Formatter("../java_tinyraytracer/file.txt");
+			Formatter form = new Formatter("../java_tinyraytracer/img.ppm");
+			form.format("%s\n", "P6");
+			form.format("%s %s\n%s\n", "256", "256", "255");
 			form.close();
 		}
 		catch (Exception e) {
