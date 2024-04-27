2f6363710d398084d7e867912b3b3f7d0b48aa9e
shobhit <shobhit32t@gmail.com>
2024-04-28 03:25:43 +0530

    Vec3f object for storing RGB values of individual pixel

---
+++ src/java_tinyraytracer/Vec3f.java
@@ -0,0 +1,11 @@
+package java_tinyraytracer;
+
+// Object type to store r, g, b values of every pixel
+public class Vec3f {
+	float r, g, b;
+	public Vec3f(float red, float green, float blue) {
+		r = red;
+		g = green;
+		b = blue;
+	}
+}
