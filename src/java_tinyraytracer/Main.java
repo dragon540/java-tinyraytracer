d36cea2b7fff8c60d6acb97eea955cf64bfde26c
shobhit <shobhit32t@gmail.com>
2024-04-28 20:08:12 +0530

    set width and height of the file through parameter and fill background witn cyan colour

--- src/java_tinyraytracer/Main.java
+++ src/java_tinyraytracer/Main.java
@@ -3,7 +3,6 @@ package java_tinyraytracer;
 public class Main {
 	public static void main(String[]args) {
 		Rendering render = new Rendering();
-		render.createPPMFile(256, 256);
-		render.renderToFile();
+		render.renderToFile(512, 512);
 	}
 }
--- src/java_tinyraytracer/Rendering.java
+++ src/java_tinyraytracer/Rendering.java
@@ -20,28 +20,42 @@ public class Rendering {
 		}
 	}
 	
-	public void renderToFile() {
-		createPPMFile(256, 256);
+	public void renderToFile(int width, int height) {
+		createPPMFile(width, height);
 		try {
 			FileWriter fwrite = new FileWriter("../java_tinyraytracer/img.ppm", true);
 			BufferedWriter bwrite = new BufferedWriter(fwrite);
 			
 			
-			  ArrayList<Vec3f> framebuffer = new ArrayList<Vec3f>(256*256);
+			ArrayList<Vec3f> framebuffer = new ArrayList<Vec3f>(256*256);
 			  
-			  for(int i=0;i<256*256;i++) {
-				  framebuffer.add(new Vec3f(0, 0, 100));
-			  }
-			 
+			/***for(int i=0;i<height*width;i++) {
+				framebuffer.add(new Vec3f(0, 100, 0));
+			}***/
 			
-			for (int i = 0; i < 256*256; ++i) {
+			for (int j = 0; j<width; j++) {
+		        for (int i = 0; i<height; i++) {\
+		        	// fill all the pixel with (0, 127, 127) i.e cyan
+		        	framebuffer.add(i+j*256, new Vec3f(0, 127, 127));
+		            //framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width), 0);
+		        }
+		    }
+			
+			
+			/***for (int i = 0; i < height*width; i++) {
+		        for (int j = 0; j<3; j++) {
+		            bwrite.write((char) (int)( Math.max(0.f, Math.min(1.f, (framebuffer.get(i)).rgb[j]))));
+		        }
+		    }***/
+			
+			for (int i = 0; i < height*width; i++) {
 		        for (int j = 0; j<3; j++) {
 		            bwrite.write((char) (int)(framebuffer.get(i)).rgb[j]);
 		        }
 		    }
 			
 			
-			/****for(int i=0;i<256*256;i++) {
+			/****for(int i=0;i<height*width;i++) {
 				for(int j=0;j<3;j++) {
 					if(i<(256*256)/2) {
 						if(j==0) {
@@ -72,7 +86,7 @@ public class Rendering {
 			
 		}
 		catch (Exception e) {
-			
+			System.out.println("Cannot open img.ppm to write");
 		}
 		
 	}
